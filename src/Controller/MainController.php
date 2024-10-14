d4d81c8c821e6faa2f646993439f91b78a33fd47
Seigneur Necron <sgnrnecron@gmail.com>
2024-10-14 16:41:05 +0200

    Controller, twig with inheritance :)

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'main_home')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/test', name: 'main_test')]
+    public function test(): Response
+    {
+        return $this->render('main/test.html.twig');
+    }
+}
