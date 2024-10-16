fc0563f61beac14de06b0b89b41bb234be5f5dd8
Seigneur Necron <sgnrnecron@gmail.com>
2024-10-17 01:26:18 +0200

    fixed Unique constraint -> UniqueEntity reordered Attributes

--- src/Entity/AbstractNamedEntity.php
+++ src/Entity/AbstractNamedEntity.php
@@ -3,13 +3,15 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[UniqueEntity('name', message: "This name is already in use")]
 abstract class AbstractNamedEntity extends AbstractNameableEntity {
 
+    #[ORM\Column(length: 255, unique: true)]
     #[Assert\NotBlank(message: "Please provide a name")]
     #[Assert\Length(max: 255, maxMessage: "That name is too long")]
-    #[ORM\Column(length: 255, unique: true)]
     protected ?string $name = null;
 
     public function getName(): ?string {
--- src/Entity/Element.php
+++ src/Entity/Element.php
@@ -9,9 +9,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: ElementRepository::class)]
 class Element extends AbstractNamedEntity {
 
+    #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: "Please provide a color")]
     #[Assert\Length(max: 255, maxMessage: "That color is too long")]
-    #[ORM\Column(length: 255)]
     private ?string $color = null;
 
     public function getColor(): ?string {
