22827bb0a4c18d1ce80f5985e93aab8cc3ef33df
Seigneur Necron <sgnrnecron@gmail.com>
2024-10-22 02:52:21 +0200

    Fixed images assets not being copied to build.

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,6 @@ import './bootstrap.js';
  */
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+
+const imagesContext = require.context('./images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
