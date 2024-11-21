a3a62da49e3a3af7e6b7e24d5d47e8272e4dc5c4
Seigneur Necron <sgnrnecron@gmail.com>
2024-11-21 23:21:22 +0100

    Fixed ArtifactSetType form entity class.

--- src/Form/Entity/ArtifactSetType.php
+++ src/Form/Entity/ArtifactSetType.php
@@ -2,19 +2,19 @@
 
 namespace App\Form\Entity;
 
-use App\Entity\Final\Element;
+use App\Entity\Final\ArtifactSet;
 use App\Form\Base\AbstractEntityType;
 use App\Form\Field\NameAndSlugType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
- * @template-extends AbstractEntityType<Element>
+ * @template-extends AbstractEntityType<ArtifactSet>
  */
 class ArtifactSetType extends AbstractEntityType {
 
     public function __construct() {
-        parent::__construct(Element::class);
+        parent::__construct(ArtifactSet::class);
     }
 
     protected function doBuildForm(FormBuilderInterface $builder, array $options): void {
